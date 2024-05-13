zyt-zsn/AutoHotkey

commit e06bcb7ac9433f84bdab0786b31426feb8c6f11e

Changed debugger to suppress exceptions during property evaluation.

source/Debugger.h
@@ -190,6 +190,7 @@ public:
 	void Exit(ExitReasons aExitReason, char *aCommandName=NULL); // Called when exiting AutoHotkey.
 	inline bool IsConnected() { return mSocket != INVALID_SOCKET; }
 	inline bool IsStepping() { return mInternalState >= DIS_StepInto; }
+	inline bool IsAtBreak() { return mInternalState == DIS_Break; }
 	inline bool HasStdErrHook() { return mStdErrMode != SR_Disabled; }
 	inline bool HasStdOutHook() { return mStdOutMode != SR_Disabled; }
 	inline bool BreakOnExceptionIsEnabled() { return mBreakOnException; }

source/error.cpp
@@ -143,7 +143,11 @@ ResultType Line::SetThrownToken(global_struct &g, ResultToken *aToken, ResultTyp
 {
 #ifdef CONFIG_DEBUGGER
 	if (g_Debugger.IsConnected())
-		if (g_Debugger.PreThrow(aToken) && !(g.ExcptMode & EXCPTMODE_CATCH))
+		if (g_Debugger.IsAtBreak() || g_Debugger.PreThrow(aToken) && !(g.ExcptMode & EXCPTMODE_CATCH))
+			// IsAtBreak() indicates the debugger was already in a break state, likely
+			// processing a property_get or context_get which caused script execution.
+			// In that case, silence all error dialogs and don't set g.ThrownToken since
+			// the debugger is lax about detecting/clearing it.  If PreThrow was called:
 			// The debugger has entered (and left) a break state, so the client has had a
 			// chance to inspect the exception and report it.  There's nothing in the DBGp
 			// spec about what to do next, probably since PHP would just log the error.
@@ -250,6 +254,11 @@ ResultType Script::RuntimeError(LPCTSTR aErrorText, LPCTSTR aExtraInfo, ResultTy
 	ASSERT(aErrorText);
 	if (!aExtraInfo)
 		aExtraInfo = _T("");
+
+#ifdef CONFIG_DEBUGGER
+	if (g_Debugger.IsAtBreak()) // i.e. the debugger is processing a property_get or context_get.
+		return FAIL; // Silent abort, no g->ThrownToken.
+#endif
 
 	if ((g->ExcptMode || mOnError.Count()
 #ifdef CONFIG_DEBUGGER

commit ac64f5b1e30e06f8bbe52988f7e77ecc284fa436

Fixed DBGp property evaluation causing Try without Catch to fail.
Broken by e06bcb7a.

source/Debugger.cpp
@@ -281,6 +281,13 @@ int Debugger::ProcessCommands(LPCSTR aBreakReason)
 		if (err = EnterBreakState(aBreakReason))
 			return err;
 	mProcessingCommands = true;
+	// Set a non-zero ExcptMode so that UnhandledException will be called for runtime
+	// errors, and use EXCPTMODE_DEBUGGER so that UnhandledException will immediately
+	// free the exception and return FAIL without doing any reporting.
+	// Any ExcptMode flags of the interrupted thread don't apply to code executed by the
+	// debugger, since it isn't executing within or being called by the current block.
+	auto excptmode = g->ExcptMode;
+	g->ExcptMode = EXCPTMODE_DEBUGGER;
 
 	// Disable notification of READ readiness and reset socket to synchronous mode.
 	u_long zero = 0;
@@ -380,6 +387,7 @@ int Debugger::ProcessCommands(LPCSTR aBreakReason)
 	}
 	ASSERT(mInternalState != DIS_Break);
 	mProcessingCommands = false;
+	g->ExcptMode = excptmode;
 	// Register for message-based notification of data arrival.  If a command
 	// is received asynchronously, control will be passed back to the debugger
 	// to process it.  This allows the debugger engine to respond even if the
@@ -1757,9 +1765,6 @@ int Debugger::ParsePropertyName(LPCSTR aFullName, int aDepth, int aVarScope, Exp
 		}
 		auto result = this_obj->Invoke(aResult.value, invoke_flags, name, t_this, param, param_count);
 
-		if (g->ThrownToken)
-			g_script.FreeExceptionToken(g->ThrownToken);
-
 		if (aResult.value.symbol == SYM_STRING && !aResult.value.mem_to_free && aResult.value.marker != aResult.value.buf)
 		{
 			// Before releasing the target object, make a copy of the string in case it points
@@ -2969,16 +2974,9 @@ void Debugger::PropertyWriter::WriteBaseProperty(IObject *aBase)
 void Debugger::PropertyWriter::WriteDynamicProperty(LPTSTR aName)
 {
 	FuncResult result_token;
-	auto excpt_mode = g->ExcptMode;
-	g->ExcptMode |= EXCPTMODE_CATCH;
 	auto result = mProp.invokee->Invoke(result_token, IT_GET, aName, mProp.value, nullptr, 0);
-	g->ExcptMode = excpt_mode;
 	if (!result)
-	{
-		if (g->ThrownToken)
-			g_script.FreeExceptionToken(g->ThrownToken);
 		result_token.SetValue(_T("<error>"), 7);
-	}
 	mDbg.AppendPropertyName(mProp.fullname, mNameLength, CStringUTF8FromTChar(aName));
 	_WriteProperty(result_token);
 	result_token.Free();

source/Debugger.h
@@ -190,7 +190,6 @@ public:
 	void Exit(ExitReasons aExitReason, char *aCommandName=NULL); // Called when exiting AutoHotkey.
 	inline bool IsConnected() { return mSocket != INVALID_SOCKET; }
 	inline bool IsStepping() { return mInternalState >= DIS_StepInto; }
-	inline bool IsAtBreak() { return mProcessingCommands; }
 	inline bool HasStdErrHook() { return mStdErrMode != SR_Disabled; }
 	inline bool HasStdOutHook() { return mStdOutMode != SR_Disabled; }
 	inline bool BreakOnExceptionIsEnabled() { return mBreakOnException; }

source/defines.h
@@ -118,6 +118,7 @@ enum ExcptModeType {EXCPTMODE_NONE = 0
 	, EXCPTMODE_CATCH = 2 // Exception will be suppressed or caught.
 	, EXCPTMODE_DELETE = 4 // Unhandled exceptions will display ERR_ABORT_DELETE vs. ERR_ABORT.
 	, EXCPTMODE_CAUGHT = 0x10 // An exception is already being handled within a CATCH, and is not shadowed by TRY.
+	, EXCPTMODE_DEBUGGER = 0x20 // Debugger is evaluating a property and wants uncaught errors suppressed.
 };
 
 #define SEND_MODES { _T("Event"), _T("Input"), _T("Play"), _T("InputThenPlay") } // Must match the enum below.

source/error.cpp
@@ -143,12 +143,8 @@ ResultType Line::SetThrownToken(global_struct &g, ResultToken *aToken, ResultTyp
 {
 #ifdef CONFIG_DEBUGGER
 	if (g_Debugger.IsConnected())
-		if (g_Debugger.IsAtBreak() || g_Debugger.PreThrow(aToken) && !(g.ExcptMode & EXCPTMODE_CATCH))
+		if (g_Debugger.PreThrow(aToken) && !(g.ExcptMode & EXCPTMODE_CATCH))
 		{
-			// IsAtBreak() indicates the debugger was already in a break state, likely
-			// processing a property_get or context_get which caused script execution.
-			// In that case, silence all error dialogs and don't set g.ThrownToken since
-			// the debugger is lax about detecting/clearing it.  If PreThrow was called:
 			// The debugger has entered (and left) a break state, so the client has had a
 			// chance to inspect the exception and report it.  There's nothing in the DBGp
 			// spec about what to do next, probably since PHP would just log the error.
@@ -257,11 +253,6 @@ ResultType Script::RuntimeError(LPCTSTR aErrorText, LPCTSTR aExtraInfo, ResultTy
 	ASSERT(aErrorText);
 	if (!aExtraInfo)
 		aExtraInfo = _T("");
-
-#ifdef CONFIG_DEBUGGER
-	if (g_Debugger.IsAtBreak()) // i.e. the debugger is processing a property_get or context_get.
-		return FAIL; // Silent abort, no g->ThrownToken.
-#endif
 
 	if ((g->ExcptMode || mOnError.Count()
 #ifdef CONFIG_DEBUGGER
@@ -1082,7 +1073,15 @@ ResultType Script::UnhandledException(Line* aLine, ResultType aErrorType)
 	// This includes OnError callbacks explicitly called below, but also COM events
 	// and CallbackCreate callbacks that execute while MsgBox() is waiting.
 	g.ThrownToken = NULL;
-
+	
+#ifdef CONFIG_DEBUGGER
+	if (g.ExcptMode & EXCPTMODE_DEBUGGER)
+	{
+		FreeExceptionToken(token);
+		return FAIL;
+	}
+#endif
+	
 	// OnError: Allow the script to handle it via a global callback.
 	static bool sOnErrorRunning = false;
 	if (mOnError.Count() && !sOnErrorRunning)