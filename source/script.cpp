zyt-zsn/AutoHotkey

commit c5e0f2c5f71108e1be122e42e3ccfbb54c7681ae

Fix retrieving unreferenced built-in vars/functions via AHK module

source/script.cpp
@@ -332,6 +332,8 @@ Script::Script()
 	// initial Sleep(10) in ExecUntil that would otherwise occur.
 	ZeroMemory(&mNIC, sizeof(mNIC));  // Constructor initializes this, to be safe.
 	mNIC.hWnd = NULL;  // Set this as an indicator that it tray icon is not installed.
+
+	mBuiltinModule.mIsBuiltinModule = true;
 	
 	// This is done here rather than in Init() because by then CreateRootPrototypes()
 	// definitely would have already caused functions to be added to the list.
@@ -6800,7 +6802,7 @@ BuiltInFunc::BuiltInFunc(FuncEntry &bif) : BuiltInFunc(bif.mName)
 
 
 
-Func *Script::GetBuiltInFunc(LPTSTR aFuncName)
+Func *Script::GetBuiltInFunc(LPCTSTR aFuncName)
 {
 	int left, right, mid, result;
 	for (left = 0, right = _countof(g_BIF) - 1; left <= right;)
@@ -7175,26 +7177,9 @@ Var *Script::FindVar(LPCTSTR aVarName, size_t aVarNameLength, int aScope
 		if (apList) *apList = varlist;
 		if (apInsertPos) *apInsertPos = insert_pos;
 
-		if (aScope & FINDVAR_GLOBAL_FALLBACK) // Declarations shadow imported names and built-in functions.
-		{
+		if (aScope & FINDVAR_GLOBAL_FALLBACK) // Declarations shadow imported names.
 			if (Var *found = FindImportedVar(var_name))
 				return found;
-
-			if (auto *func = GetBuiltInFunc(var_name))
-			{
-				Var *var = AddVar(var_name, aVarNameLength, varlist, insert_pos, VAR_DECLARE_GLOBAL | ADDVAR_NO_VALIDATE);
-				if (!var)
-				{
-					if (aDisplayError)
-						*aDisplayError = FAIL;
-					func->Release();
-					return nullptr;
-				}
-				var->AssignSkipAddRef(func);
-				var->MakeReadOnly();
-				return var;
-			}
-		}
 	}
 
 	// Since no match was found, if this is a local fall back to searching outer functions and the
@@ -7218,14 +7203,7 @@ Var *Script::FindVar(LPCTSTR aVarName, size_t aVarNameLength, int aScope
 				return found;
 	}
 	// Otherwise, since above didn't return:
-	if (auto *builtin = GetBuiltInVar(var_name))
-	{
-		Var *var = FindOrAddBuiltInVar(var_name, builtin);
-		if (!var && aDisplayError)
-			*aDisplayError = FAIL;
-		return var;
-	}
-	return nullptr;
+	return FindOrAddBuiltInVar(var_name, aScope & FINDVAR_GLOBAL_FALLBACK, aDisplayError);
 }
 
 
@@ -7385,21 +7363,44 @@ Var *Script::AddVar(LPCTSTR aVarName, size_t aVarNameLength, VarList *aList, int
 
 
 
-Var *Script::FindOrAddBuiltInVar(LPCTSTR aVarName, VarEntry *aVarEntry)
+Var *Script::FindOrAddBuiltInVar(LPCTSTR aVarName, bool aAllowFunc, ResultType *aDisplayError)
 {
+	// mBuiltinModule currently isn't handled via mCurrentModule->mImports because we would
+	// still need to call Find() to get insert_pos if a BIV must be added, and then there's
+	// the question of what to do if a non-null value is returned even though the caller
+	// should have found it via mImports and thereby prevented this call.
 	int insert_pos;
-	if (Var *found = mBuiltinModule.mVars.Find(aVarName, &insert_pos))
-		return found;
-	LPTSTR name = SimpleHeap::Malloc(aVarName);
-	if (!name)
-		return nullptr;
-	Var *the_new_var = new Var(name, aVarEntry, VAR_DECLARE_GLOBAL | VAR_EXPORTED);
-	if (!the_new_var || !mBuiltinModule.mVars.Insert(the_new_var, insert_pos))
+	Var *var = mBuiltinModule.mVars.Find(aVarName, &insert_pos);
+	if (var)
+		return var;
+
+	Func *func = nullptr;
+	if (auto biv = GetBuiltInVar(aVarName))
 	{
-		MemoryError();
+		if (auto name = SimpleHeap::Malloc(aVarName))
+			var = new Var(name, biv, VAR_DECLARE_GLOBAL | VAR_EXPORTED);
+	}
+	else if (aAllowFunc && (func = GetBuiltInFunc(aVarName)))
+	{
+		var = new Var(const_cast<LPTSTR>(func->mName), VAR_DECLARE_GLOBAL | VAR_EXPORTED);
+	}
+	else
+		return nullptr;
+
+	if (!var || !mBuiltinModule.mVars.Insert(var, insert_pos))
+	{
+		if (func)
+			func->Release();
+		if (aDisplayError)
+			*aDisplayError = MemoryError();
 		return nullptr;
 	}
-	return the_new_var;
+	if (func)
+	{
+		var->AssignSkipAddRef(func);
+		var->MakeReadOnly();
+	}
+	return var;
 }
 
 

source/script.h
@@ -2139,9 +2139,8 @@ private:
 	FuncList mFuncs;
 
 	ScriptModuleList mModules;
-	ScriptModule mBuiltinModule { _T("AHK"), nullptr };
-	ScriptImport mDefaultImport { &mBuiltinModule };
-	ScriptModule mDefaultModule { _T("__Main"), &mDefaultImport };
+	ScriptModule mBuiltinModule { _T("AHK") };
+	ScriptModule mDefaultModule { _T("__Main") };
 	ScriptModule *mCurrentModule = &mBuiltinModule;
 	ScriptModule *mLastModule = nullptr;
 	
@@ -2332,8 +2331,8 @@ public:
 	LPTSTR FindLibraryFile(LPTSTR aName, size_t aNameLength, bool aIsModule = false);
 #endif
 	Func *FindGlobalFunc(LPCTSTR aFuncName, size_t aFuncNameLength = 0);
-	static Func *GetBuiltInFunc(LPTSTR aFuncName);
-	static Func *GetBuiltInMdFunc(LPTSTR aFuncName);
+	static Func *GetBuiltInFunc(LPCTSTR aFuncName);
+	static Func *GetBuiltInMdFunc(LPCTSTR aFuncName);
 	UserFunc *AddFunc(LPCTSTR aFuncName, size_t aFuncNameLength, FuncDefType aIsInExpression = FuncDefNormal, Object *aClassObject = nullptr);
 	Var *AddFuncVar(UserFunc *aFunc);
 	UserFunc *AddFuncToList(UserFunc *aFunc);
@@ -2362,7 +2361,7 @@ public:
 		, VarList **apList = nullptr, int *apInsertPos = nullptr, ResultType *aDisplayError = nullptr);
 	Var *FindUpVar(LPCTSTR aVarName, size_t aVarNameLength, UserFunc &aInner, ResultType *aDisplayError);
 	Var *AddVar(LPCTSTR aVarName, size_t aVarNameLength, VarList *aList, int aInsertPos, int aScope);
-	Var *FindOrAddBuiltInVar(LPCTSTR aVarName, VarEntry *aVarEntry);
+	Var *FindOrAddBuiltInVar(LPCTSTR aVarName, bool aAllowFunc, ResultType *aDisplayError);
 	static VarEntry *GetBuiltInVar(LPCTSTR aVarName);
 
 	// Alias to improve clarity and reduce code size (if compiler chooses not to inline; due to how parameter defaults work):

source/MdFunc.cpp
@@ -28,7 +28,7 @@ MdFuncEntry sMdFunc[]
 };
 
 
-Func *Script::GetBuiltInMdFunc(LPTSTR aFuncName)
+Func *Script::GetBuiltInMdFunc(LPCTSTR aFuncName)
 {
 #ifdef _DEBUG
 	static bool sChecked = false;

source/script_module.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "script.h"
+#include "globaldata.h"
 
 
 Object *ScriptModule::sPrototype;
@@ -8,6 +9,9 @@ Object *ScriptModule::sPrototype;
 ResultType ScriptModule::Invoke(IObject_Invoke_PARAMS_DECL)
 {
 	auto var = aName ? mVars.Find(aName) : nullptr;
+	if (!var && mIsBuiltinModule)
+		// This is a slight hack to support built-in vars which haven't been referenced directly.
+		var = g_script.FindOrAddBuiltInVar(aName, true, nullptr);
 	if (!var || !var->IsExported())
 		return ObjectBase::Invoke(IObject_Invoke_PARAMS);
 
@@ -31,7 +35,7 @@ ResultType Script::ParseModuleDirective(LPCTSTR aName)
 		return ScriptError(ERR_DUPLICATE_DECLARATION, aName);
 	// TODO: Validate module names.
 	aName = SimpleHeap::Alloc(aName);
-	auto mod = new ScriptModule(aName, &mDefaultImport);
+	auto mod = new ScriptModule(aName);
 	if (!mModules.Insert(mod, at))
 		return FAIL;
 	CloseCurrentModule();
@@ -152,7 +156,7 @@ ResultType Script::ResolveImports(ScriptImport &imp)
 			auto cur_mod = mCurrentModule;
 			auto last_mod = mLastModule;
 			mLastModule = nullptr; // Start a new chain.
-			imp.mod = mCurrentModule = new ScriptModule(mod_name, &mDefaultImport);
+			imp.mod = mCurrentModule = new ScriptModule(mod_name);
 			if (!LoadIncludedFile(path, false, false))
 				return FAIL;
 			if (!CloseCurrentModule())

source/script_module.h
@@ -30,6 +30,7 @@ public:
 	Var *mSelf = nullptr;
 	FileIndexType *mFiles = nullptr, mFilesCount = 0, mFilesCountMax = 0;
 	bool mExecuted = false;
+	bool mIsBuiltinModule = false;
 
 	// #Warn settings
 	WarnMode Warn_LocalSameAsGlobal = WARNMODE_OFF;
@@ -40,8 +41,7 @@ public:
 	ResultType AddFileIndex(FileIndexType aFile);
 
 	ScriptModule() {}
-	ScriptModule(LPCTSTR aName, ScriptImport *aDefaultImport)
-		: mName(aName), mImports(aDefaultImport) {}
+	ScriptModule(LPCTSTR aName) : mName(aName) {}
 
 	void *operator new(size_t aBytes) {return SimpleHeap::Alloc(aBytes);}
 	void *operator new[](size_t aBytes) {return SimpleHeap::Alloc(aBytes);}

commit c5435427d5b0e1ec35a8e62384a3a5f7eeafed59

Fix built-in classes and functions to not be super-global
Broken by c5e0f2c5.

source/script.cpp
@@ -7356,7 +7356,7 @@ Var *Script::AddVar(LPCTSTR aVarName, size_t aVarNameLength, VarList *aList, int
 
 
 
-Var *Script::FindOrAddBuiltInVar(LPCTSTR aVarName, bool aAllowFunc, ResultType *aDisplayError)
+Var *Script::FindOrAddBuiltInVar(LPCTSTR aVarName, bool aAllowNonVirtual, ResultType *aDisplayError)
 {
 	// mBuiltinModule currently isn't handled via mCurrentModule->mImports because we would
 	// still need to call Find() to get insert_pos if a BIV must be added, and then there's
@@ -7365,7 +7365,7 @@ Var *Script::FindOrAddBuiltInVar(LPCTSTR aVarName, bool aAllowFunc, ResultType *
 	int insert_pos;
 	Var *var = mBuiltinModule.mVars.Find(aVarName, &insert_pos);
 	if (var)
-		return var;
+		return (aAllowNonVirtual || var->Type() == VAR_VIRTUAL) ? var : nullptr;
 
 	Func *func = nullptr;
 	if (auto biv = GetBuiltInVar(aVarName))
@@ -7373,7 +7373,7 @@ Var *Script::FindOrAddBuiltInVar(LPCTSTR aVarName, bool aAllowFunc, ResultType *
 		if (auto name = SimpleHeap::Malloc(aVarName))
 			var = new Var(name, biv, VAR_DECLARE_GLOBAL | VAR_EXPORTED);
 	}
-	else if (aAllowFunc && (func = GetBuiltInFunc(aVarName)))
+	else if (aAllowNonVirtual && (func = GetBuiltInFunc(aVarName)))
 	{
 		var = new Var(const_cast<LPTSTR>(func->mName), VAR_DECLARE_GLOBAL | VAR_EXPORTED);
 	}

source/script.h
@@ -2362,7 +2362,7 @@ public:
 		, VarList **apList = nullptr, int *apInsertPos = nullptr, ResultType *aDisplayError = nullptr);
 	Var *FindUpVar(LPCTSTR aVarName, size_t aVarNameLength, UserFunc &aInner, ResultType *aDisplayError);
 	Var *AddVar(LPCTSTR aVarName, size_t aVarNameLength, VarList *aList, int aInsertPos, int aScope);
-	Var *FindOrAddBuiltInVar(LPCTSTR aVarName, bool aAllowFunc, ResultType *aDisplayError);
+	Var *FindOrAddBuiltInVar(LPCTSTR aVarName, bool aAllowNonVirtual, ResultType *aDisplayError);
 	static VarEntry *GetBuiltInVar(LPCTSTR aVarName);
 
 	// Alias to improve clarity and reduce code size (if compiler chooses not to inline; due to how parameter defaults work):