d137d7786e4a7b54442cb277e4651374c48c7c92
zyt-zsn <zyt_xl@163.com>
2024-10-23 09:13:57 +0800

    Prioritise package build dir over system path in finding executable.

--- emms-volume-winvol.el
+++ emms-volume-winvol.el
@@ -1,9 +1,31 @@
 ;; https://lists.defectivebydesign.org/archive/html/emms-help/2019-01/msg00023.html
 ;; change Windows volume using AutoHotKey script WinVol
-(defcustom emms-winvol-program "WinVol.exe"
+(defcustom emms-winvol-program
+  (let ((executable (if (eq system-type 'windows-nt)
+                        "WinVol.exe" "WinVol"))
+        (default-directory
+         (or (and load-file-name
+                  (file-name-directory load-file-name))
+             default-directory)))
+    (cl-labels ((try-directory (directory)
+                  (and (file-directory-p directory)
+                       (file-executable-p (expand-file-name executable directory))
+                       (expand-file-name executable directory))))
+      (or (executable-find executable)
+          ;; This works if epdfinfo is in the same place as emacs and
+          ;; the editor was started with an absolute path, i.e. it is
+          ;; meant for Windows/Msys2.
+          (and (stringp (car-safe command-line-args))
+               (file-name-directory (car command-line-args))
+               (try-directory
+                (file-name-directory (car command-line-args))))
+          ;; If we are running directly from the git repo.
+          (try-directory (expand-file-name "../server"))
+          ;; Fall back to epdfinfo in the directory of this file.
+          (expand-file-name executable))))
   "Name, and possibly location, of the WinVol executable"
   :group 'emms-volume
-  :type 'string)
+  :type 'file)
 
 ;;;###autoload
 (defun emms-volume-winvol-change (amount)
@@ -13,7 +35,7 @@
     (setq amount (number-to-string amount)))
   (message "Current playback: %s"
 		   (with-temp-buffer
-			 (progn (zerop (shell-command (concat (expand-file-name (executable-find emms-winvol-program))
+			 (progn (zerop (shell-command (concat emms-winvol-program
 												  " " amount)
 										  (current-buffer)))
 					(if (progn (goto-char (point-min))
